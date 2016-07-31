879337b82988b91245f9263e6cf90fd84007fb55
Ryan Mulkowsky <ryanmulkowsky@gmail.com>
2016-07-31 17:23:13 -0400

    added modals for questions

---
+++ js/app.js
@@ -0,0 +1,143 @@
+// Modal - Cell 0
+var modal = document.getElementById('myModal0');
+var btn = document.getElementById("my-btn0");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Modal - Cell 1
+var modal = document.getElementById('myModal1');
+var btn = document.getElementById("my-btn1");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Modal - Cell 2
+var modal = document.getElementById('myModal2');
+var btn = document.getElementById("my-btn2");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Modal - Cell 3
+var modal = document.getElementById('myModal3');
+var btn = document.getElementById("my-btn3");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Modal - Cell 4
+var modal = document.getElementById('myModal4');
+var btn = document.getElementById("my-btn4");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Modal - Cell 5
+var modal = document.getElementById('myModal5');
+var btn = document.getElementById("my-btn5");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Modal - Cell 6
+var modal = document.getElementById('myModal6');
+var btn = document.getElementById("my-btn6");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Modal - Cell 7
+var modal = document.getElementById('myModal7');
+var btn = document.getElementById("my-btn7");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Modal - Cell 8
+var modal = document.getElementById('myModal8');
+var btn = document.getElementById("my-btn8");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
