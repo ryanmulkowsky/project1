fef06172e8842696c2b073333dbb901f69cb38ea
Ryan Mulkowsky <ryanmulkowsky@gmail.com>
2016-08-01 15:16:11 -0400

    added agree/disagree buttons, added questions to modals

--- js/app.js
+++ js/app.js
@@ -3,10 +3,16 @@ var modal = document.getElementById('myModal0');
 
 var btn = document.getElementById("myBtn0");
 
+var ag = document.getElementById("myAg0");
+
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+ag.onclick = function() {
+    modal.style.display = "none";
 }
 
 span.onclick = function() {
@@ -24,10 +30,16 @@ var modal = document.getElementById('myModal1');
 
 var btn = document.getElementById("myBtn1");
 
+var ag = document.getElementById("myAg1");
+
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+ag.onclick = function() {
+    modal.style.display = "none";
 }
 
 span.onclick = function() {
@@ -45,10 +57,16 @@ var modal = document.getElementById('myModal2');
 
 var btn = document.getElementById("myBtn2");
 
+var ag = document.getElementById("myAg2");
+
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+ag.onclick = function() {
+    modal.style.display = "none";
 }
 
 span.onclick = function() {
@@ -66,10 +84,16 @@ var modal = document.getElementById('myModal3');
 
 var btn = document.getElementById("myBtn3");
 
+var ag = document.getElementById("myAg3");
+
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+ag.onclick = function() {
+    modal.style.display = "none";
 }
 
 span.onclick = function() {
@@ -87,10 +111,16 @@ var modal = document.getElementById('myModal4');
 
 var btn = document.getElementById("myBtn4");
 
+var ag = document.getElementById("myAg4");
+
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+ag.onclick = function() {
+    modal.style.display = "none";
 }
 
 span.onclick = function() {
@@ -108,10 +138,16 @@ var modal = document.getElementById('myModal5');
 
 var btn = document.getElementById("myBtn5");
 
+var ag = document.getElementById("myAg5");
+
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+ag.onclick = function() {
+    modal.style.display = "none";
 }
 
 span.onclick = function() {
@@ -129,10 +165,16 @@ var modal = document.getElementById('myModal6');
 
 var btn = document.getElementById("myBtn6");
 
+var ag = document.getElementById("myAg6");
+
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+ag.onclick = function() {
+    modal.style.display = "none";
 }
 
 span.onclick = function() {
@@ -150,10 +192,16 @@ var modal = document.getElementById('myModal7');
 
 var btn = document.getElementById("myBtn7");
 
+var ag = document.getElementById("myAg7");
+
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+ag.onclick = function() {
+    modal.style.display = "none";
 }
 
 span.onclick = function() {
@@ -171,10 +219,16 @@ var modal = document.getElementById('myModal8');
 
 var btn = document.getElementById("myBtn8");
 
+var ag = document.getElementById("myAg8");
+
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+ag.onclick = function() {
+    modal.style.display = "none";
 }
 
 span.onclick = function() {
